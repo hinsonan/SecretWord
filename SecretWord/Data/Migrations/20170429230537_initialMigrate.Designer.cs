3fb76cbd00fdb0d500384e475d6c3e152c8473c1
hinsonan <hinsonan@gmail.com>
2017-04-29 18:13:30 -0500

    creates the DB

--- SecretWord/Data/Migrations/20170429230537_initialMigrate.Designer.cs
+++ SecretWord/Data/Migrations/20170429230537_initialMigrate.Designer.cs
@@ -8,9 +8,10 @@ using SecretWord.Data;
 namespace SecretWord.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170429230537_initialMigrate")]
+    partial class initialMigrate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
