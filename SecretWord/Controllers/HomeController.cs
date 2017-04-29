4fdb473e1d1ffd453c9e3693d423912fdc960872
hinsonan <hinsonan@gmail.com>
2017-04-29 18:02:30 -0500

    adds the model that will be used to make DB

--- SecretWord/Controllers/HomeController.cs
+++ SecretWord/Controllers/HomeController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SecretWord.Data;
 
 namespace SecretWord.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            db = context;
+        }
         public IActionResult Index()
         {
             return View();
---
+++ SecretWord/Models/SecretWordModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecretWord.Models
+{
+    public class SecretWordModel
+    {
+        public int ID { get; set; }
+        public string Word { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Username { get; set; }
+    }
+}
