a63281d51c500e971d530742f4c43b6f01e07207
hinsonan <hinsonan@gmail.com>
2017-05-03 22:06:23 -0500

    grabs the most recent word in database

--- SecretWord/Controllers/HomeController.cs
+++ SecretWord/Controllers/HomeController.cs
@@ -6,28 +6,34 @@ using Microsoft.AspNetCore.Mvc;
 using SecretWord.Data;
 using SecretWord.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace SecretWord.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> user;
 
-        public HomeController(ApplicationDbContext context)
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userman)
         {
             db = context;
+            user = userman;
         }
         public IActionResult Index()
         {
-            var words = db.SecretWord                
-                .OrderBy(am => am.TimeStamp);
+            var word = db.SecretWord                
+                .OrderByDescending(am => am.TimeStamp).First();
             
-            return View(words);
+            return View(word);
         }
 
         public IActionResult AddWord()
         {
-
+            SecretWordModel newWord = new SecretWordModel();
+            newWord.TimeStamp = DateTime.Now;
+            newWord.Username = User.Identity.Name;
+            // newWord.Word = 
 
             return Redirect(@"/Home/Index");
         }
